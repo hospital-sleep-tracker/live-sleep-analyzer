2c7f54fc5876cad6093eb4ea153e70abd7eb2fbc
Dan Osborne <djosborne10@gmail.com>
2015-02-02 23:30:49 -0500

    got rid of outdated code

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-apt-get install -Y libusb git virtualenv python
-git reset hard origin/master
-source ~/Virtualenvs/hospital-sleep-tracker/bin/activate
-pip install -r requirements.txt
-while :
-do
-  python ./main.py
-done

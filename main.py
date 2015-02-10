82c1f0c53c432d67ab56fe7845d4bb5854ab098b
Dan Osborne <djosborne10@gmail.com>
2015-02-10 00:46:00 -0500

    refactored into basic classes. needs work

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def main():
         global numpy, pyplot, animation
         import numpy
         from matplotlib import pyplot, animation
-        graph = Graph()
+        graph = LazyGraph()
 
     # Create logs directory.
     try:
@@ -44,80 +44,106 @@ def main():
             log.error("Can't create logs directory")
             sys.exit(1)
 
-    while True:
+    try:
+        # We must initialize
+        if args.filename:
+            sleep_reader = InFile(args.filename)
+            indicator_led = DummyLightSwitch()
+            logfile = DummyLogFile()
+        else:
+            sleep_reader = Teensy()
+            indicator_led = LightSwitch()
+            logfile = OutFile()
+            indicator_led.turn_on()
+
+        analyzer = Analyzer()
+
         try:
-            # Keep Searching / initializing USB reader until we find it
-            teensy = None
-            while teensy == None:
-                teensy = get_teensy_usb()
+            while sleep_reader.is_ready():
+                # Read value from accelerometer
+                movement_value = sleep_reader.get_next_movement_value()
+                if movement_value is None:
+                    # Got bad value from reader. Move on. Note: Index will still be incremented
+                    indicator_led.turn_off()
+                    continue
+                else:
+                    indicator_led.turn_on()
 
-            # Open logfile for this session
-            try:
-                logfile_name = 'logs/%s.slp.csv' % datetime.datetime.now().strftime("%m%d%Y_%H%M%S")
-                log.info("Logging to %s" % logfile_name)
-                logfile = open(logfile_name, 'a')
-                logwriter = csv.writer(logfile)
-                turn_on_led()
+                if graph:
+                    graph.add(movement_value)
+                logfile.record_value(movement_value)
+                analyzer.track_new_value(movement_value)
 
-
-                # Write CSV header information
-                logwriter.writerow(['Date', 'Time', 'Reading Index', 'Movement Value'])
-            except:
-                log.error("Unable to open logfile. Quitting")
-                sys.exit(1)
-
-            try:
-                index = -1
-                while teensy.isOpen():
-                    index += 1
-                    # Read value from accelerometer
-                    movement_value = get_movement_value(teensy)
-                    if movement_value is None:
-                        # Got bad value from reader. Move on. Note: Index will still be incremented
-                        turn_off_led()
-                        continue
-
-                    # Get a timestamp of this reading
-                    turn_on_led()
-                    timestamp = datetime.datetime.now()
-                    date, time = str(timestamp).split(' ')
-
-                    if graph:
-                        graph.add(movement_value)
-                    if logfile:
-                        logwriter.writerow([index, date, time, movement_value])
-            except KeyboardInterrupt:
-                log.info("Interrupt detected. Closing logfile and quitting")
-                logfile.close()
-                turn_off_led()
-                sys.exit(0)
-            except serial.SerialException:
-                log.info("USB Error. Closing everything")
-                turn_off_led()
-                logfile.close()
-                teensy.close()
         except KeyboardInterrupt:
-            log.info("Interrupt detected. Quitting")
+            log.info("Interrupt detected. Closing logfile and quitting")
+            logfile.close()
+            indicator_led.turn_off()
             sys.exit(0)
-        except Exception as e:
-            log.error("Encountered unexpected exception: %s" % e)
-            teensy = None
+        except serial.SerialException:
+            log.info("USB Error. Closing everything")
+            indicator_led.turn_off()
+            logfile.close()
+            sleep_reader.close()
+        except EOFError:
+            log.info("Done reading file!")
+            if graph:
+                graph.show()
+            input("Press Enter to continue...")
+    except KeyboardInterrupt:
+        log.info("Interrupt detected. Quitting")
+        sys.exit(0)
+    except Exception as e:
+        log.error("Encountered unexpected exception: %s" % e)
+        sleep_reader = None
 
-def turn_on_led():
-    try:
-        with open('/sys/class/leds/ACT/brightness', 'w') as f:
-            f.write('1')
-    except IOError:
-        log.warning("Unable to change indicator led")
+class Analyzer(object):
+    def __init__(self):
+        self.values = []
+        self.rem_sleep = True
 
-def turn_off_led():
-    try:
-        with open('/sys/class/leds/ACT/brightness', 'w') as f:
-            f.write('0')
-    except IOError:
-        log.warning("Unable to change indicator led")
+    def track_new_value(self, value):
+        self.values.append(value)
+
+
+class LightSwitch(object):
+    """Used to turn the raspberry pi indicator light on and off.
+    The class is necessary to hold state of whether the light is currently on or off.
+    It is much faster to save state then do an arbitrary hardware write every time.
+    """
+    def __init__(self):
+        self._is_on = False
+
+    def turn_on(self):
+        """Turns the light on, ONLY if it is currently off"""
+        if not self._is_on:
+            try:
+                with open('/sys/class/leds/ACT/brightness', 'w') as f:
+                    f.write('1')
+                self._is_on = True
+            except IOError:
+                log.warning("Unable to change indicator led")
+
+    def turn_off(self):
+        """Turns the ligh off, ONLY if it is currently on"""
+        if self._is_on:
+            try:
+                with open('/sys/class/leds/ACT/brightness', 'w') as f:
+                    f.write('0')
+                self._is_on = False
+            except IOError:
+                log.warning("Unable to change indicator led")
+
+
+class DummyLightSwitch(object):
+    def turn_on(self):
+        pass
+
+    def turn_off(self):
+        pass
+
 
 class Graph(object):
+    """Graphs data as it is supplied"""
     def __init__(self):
         pyplot.ion()
         self.data = [0] * 50
@@ -130,96 +156,149 @@ class Graph(object):
         self.line.set_data(range(0,50), self.data)
         pyplot.draw()
 
-def get_movement_value(teensy):
-    """Gets new data from the provided teensy object (via readline)
-    and converts it into an integer.
+    def show(self):
+        pass
 
-    :returns:
-        Integer value representing movement as determined by teensy, or None
-    """
-    raw_value = teensy.readline().rstrip()
-    if raw_value == '':
-        log.warning("Teensy returned empty string. Ignoring")
-        return
-    else:
+class LazyGraph(object):
+    def __init__(self):
+        pyplot.ion()
+        self.data = []
+        self.ax = pyplot.axes(xlim=(0, 50), ylim=(0, 200))
+        self.line, = self.ax.plot(self.data, lw=2)
+
+    def add(self, movement_value):
+        self.data.append(movement_value)
+
+    def show(self):
+        pyplot.ylim((0, 200))
+        pyplot.xlim((0, len(self.data)))
+        self.line.set_data(range(0,len(self.data)), self.data)
+        pyplot.draw()
+
+class InputDevice(object):
+    def __init__(self, filename):
+        pass
+
+    def get_next_movement_value(self):
+        raise NotImplementedError()
+
+    def is_ready(self):
+        return True
+
+
+class Teensy(InputDevice):
+    def __init__(self):
+        self.teensy = None
+        """Serial object"""
+
+        while self.teensy is None:
+            self.teensy = self._get_teensy_usb()
+
+    def _get_teensy_usb(self):
+        """Searches, opens, and returns a serial port object connected to the teensy
+
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            An initialized serial object (hopefully) connected to the teensy
+        """
+        if sys.platform.startswith('win'):
+            log.debug("Using windows system.")
+            ports = ['COM' + str(i + 1) for i in range(256)]
+
+        elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+            # this is to exclude your current terminal "/dev/tty"
+            log.debug("Using linux system")
+            ports = glob.glob('/dev/tty[A-Za-z]*')
+
+        elif sys.platform.startswith('darwin'):
+            log.debug("Using darwin (Apple) system")
+            ports = glob.glob('/dev/tty.*')
+
+        else:
+            log.error("Unsupported / uncrecognized platform")
+            raise EnvironmentError('Unsupported platform')
+
+        for port in ports:
+            log.debug("Found available port: %s" % port)
+
+        # Method 1: Match via ready-to-read serial ports
+        ready_usb_devices = []
+        for port in ports:
+            log.info("Checking: %s" % port)
+            try:
+                teensy = serial.Serial(port=port, timeout=1)
+                if teensy.read(4) != '':
+                    log.info("Using %s" % port)
+                    return teensy
+            except (OSError, serial.SerialException):
+                pass
+
+    def get_next_movement_value():
+        """Gets new data from the provided teensy object (via readline)
+        and converts it into an integer.
+
+        :returns:
+            Integer value representing movement as determined by teensy, or None
+        """
+        raw_value = self.teensy.readline().rstrip()
+        if raw_value == '':
+            log.warning("Teensy returned empty string. Ignoring")
+            return
+        else:
+            try:
+                movement_value = int(raw_value)
+                log.info("Read movement value: %d" % movement_value)
+                return movement_value
+            except:
+                log.warning("(Ignoring) Couldn't convert following value to integer: %s" % raw_value)
+
+    def is_ready():
+        return self.teensy.isOpen()
+
+class InFile(InputDevice):
+    def __init__(self, filename):
         try:
-            movement_value = int(raw_value)
-            log.info("Read movement value: %d" % movement_value)
-            return movement_value
+            self._file = open(filename, 'r')
+            header = self._file.readline()
+
         except:
-            log.warning("(Ignoring) Couldn't convert following value to integer: %s" % raw_value)
+            log.error("Couldn't open input file.")
+            sys.exit(1)
 
-def get_teensy_usb():
-    """Searches, opens, and returns a serial port object connected to the teensy
+    def get_next_movement_value(self):
+        line = self._file.readline()
+        if line == '':
+            raise EOFError
 
-    :raises EnvironmentError:
-        On unsupported or unknown platforms
-    :returns:
-        An initialized serial object (hopefully) connected to the teensy
-    """
-    if sys.platform.startswith('win'):
-        log.debug("Using windows system.")
-        ports = ['COM' + str(i + 1) for i in range(256)]
+        else:
+            vals = line.split(",")
+            return int(vals[-1].strip())
 
-    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-        # this is to exclude your current terminal "/dev/tty"
-        log.debug("Using linux system")
-        ports = glob.glob('/dev/tty[A-Za-z]*')
 
-    elif sys.platform.startswith('darwin'):
-        log.debug("Using darwin (Apple) system")
-        ports = glob.glob('/dev/tty.*')
-
-    else:
-        log.error("Unsupported / uncrecognized platform")
-        raise EnvironmentError('Unsupported platform')
-
-    for port in ports:
-        log.debug("Found available port: %s" % port)
-
-    # Method 1: Match via ready-to-read serial ports
-    ready_usb_devices = []
-    for port in ports:
-        log.info("Checking: %s" % port)
+class OutFile(object):
+    def __init__(self):
+        self.index = -1
+        logfile_name = 'logs/%s.slp.csv' % datetime.datetime.now().strftime("%m%d%Y_%H%M%S")
+        log.info("Logging to %s" % logfile_name)
         try:
-            teensy = serial.Serial(port=port, timeout=1)
-            if teensy.read(4) != '':
-                log.info("Using %s" % port)
-                return teensy
-        except (OSError, serial.SerialException):
-            pass
+            self.logfile = open(logfile_name, 'a')
+            self.logwriter = csv.writer(logfile)
+        except:
+            log.error("Unable to open logfile. Quitting")
+            sys.exit(1)
+        # Write CSV header information
+        logwriter.writerow(['Date', 'Time', 'Reading Index', 'Movement Value'])
 
-    # Method 2: Match via vendor USB info
-    # device = None
-    # while dev == None:
-    #     time.sleep(1)
-    #     device = usb.core.find(idVendor=TEENSY_VENDOR_ID, idProduct=TEENSY_PRODUCT_ID)
+    def record_value(self, value):
+        timestamp = datetime.datetime.now()
+        date, time = str(timestamp).split(' ')
+        logwriter.writerow([self.index, date, time, value])
+        self.index += 1
 
-    # if device.is_kernel_driver_active(0):
-    #     try:
-    #         device.detach_kernel_driver(0)
-    #     except usb.core.USBError as e:
-    #         log.error("Could not detach kernel driver %s" % str(e))
-    #         device = None
-
-    # try:
-    #     device.set_configuration()
-    #     device.reset()
-    # except usb.core.USBError as e:
-    #     sys.exit("Could not set configuration: %s" % str(e))
-
-    # for port in ports:
-    #     try:
-    #         log.info("Checking port %s" % port)
-    #         serial_device = serial.Serial(port=port, timeout=2)
-    #         test_line = serial_device.readline()
-    #         if test_line == '':
-    #             raise OSError
-    #         serial_device.close()
-    #         ready_usb_devices.append(serial_device)
-    #     except (OSError, serial.SerialException):
-    #         log.debug("Port %s unavailable")
-    #         pass
+class DummyLogFile(object):
+    def record_value(self, value):
+        pass
 
 if __name__ == "__main__":
     main()

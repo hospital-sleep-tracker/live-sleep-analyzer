d7a91fc4b29053c3d3a0097b2362d0c699169d97
Dan Osborne <djosborne10@gmail.com>
2015-04-02 17:58:51 -0400

    made changes to onboot_run

--- onboot_run.sh
+++ onboot_run.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+# This script auto-updates, and runs the slep-logger application for use on the pi
+# This script should be run by /etc/rc.local
+# Add a line at the bottom that reads: screen -d -m /home/pi/live-sleep-analyzer/onboot_run.sh
+
 echo none >/sys/class/leds/led0/trigger
 
 if ping -c 1 archive.raspberrypi.org &> /dev/null; then
   apt-get update
   apt-get upgrade -y --force-yes
-  apt-get install -y screen python-usb python-serial python-matplotlib vsftpd
+  apt-get install -y screen python-usb python-serial python-matplotlib python-sklearn vsftpd
 fi
 
 cd /home/pi
@@ -22,7 +26,7 @@ fi
 git reset --hard origin/master
 while :
 do
-  python sleep-logger.py
+  python pysleep/sleep-logger.py
 done
 
 exit 1

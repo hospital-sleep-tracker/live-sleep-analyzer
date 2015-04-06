26628251a1a060771b692b27d01fbc35b9971439
Dan Osborne <djosborne10@gmail.com>
2015-04-06 09:48:56 -0400

    updated accelerometer libraries as well as our homebrew code

--- arduino/Accelerometer1/ADXL362.cpp
+++ arduino/Accelerometer1/ADXL362.cpp
@@ -9,18 +9,21 @@
  
  Created June 2012
  by Anne Mahaffey - hosted on http://annem.github.com/ADXL362
+
+ Modified May 2013
+ by Jonathan Ruiz de Garibay
  
  */ 
 
 #include <Arduino.h>
-#include <ADXL362.h>
+#include "ADXL362.h"
 #include <SPI.h>
 
-const int slaveSelectPin = 10;
-const bool debugSerial = 0;
+//#define ADXL362_DEBUG
+
+int16_t slaveSelectPin = 10;
 
 ADXL362::ADXL362() {
-
 }
 
 
@@ -28,94 +31,119 @@ ADXL362::ADXL362() {
 //  begin()
 //  Initial SPI setup, soft reset of device
 //
-void ADXL362::begin() {
+void ADXL362::begin(int16_t chipSelectPin) {
+  slaveSelectPin = chipSelectPin;
   pinMode(slaveSelectPin, OUTPUT);
   SPI.begin();
-  SPI.setDataMode(SPI_MODE0);	//CPHA = CPOL = 0    MODE = 0
+  SPI.setDataMode(SPI_MODE0); //CPHA = CPOL = 0    MODE = 0
   delay(1000);
     
   // soft reset
   SPIwriteOneRegister(0x1F, 0x52);  // Write to SOFT RESET, "R"
   delay(10);
+#ifdef ADXL362_DEBUG
   Serial.println("Soft Reset\n");
+#endif
  }
-
  
 //
 //  beginMeasure()
 //  turn on Measurement mode - required after reset
 // 
 void ADXL362::beginMeasure() {
-  byte temp = SPIreadOneRegister(0x2D);	// read Reg 2D before modifying for measure mode
-  if (debugSerial) {Serial.print(  "Setting Measeurement Mode - Reg 2D before = "); Serial.print(temp); }
+  byte temp = SPIreadOneRegister(0x2D); // read Reg 2D before modifying for measure mode
+
+#ifdef ADXL362_DEBUG
+  Serial.print(  "Setting Measeurement Mode - Reg 2D before = "); 
+  Serial.print(temp); 
+#endif
 
   // turn on measurement mode
-  byte tempwrite = temp | 0x02;			// turn on measurement bit in Reg 2D
+  byte tempwrite = temp | 0x02;     // turn on measurement bit in Reg 2D
   SPIwriteOneRegister(0x2D, tempwrite); // Write to POWER_CTL_REG, Measurement Mode
-  delay(10);	
+  delay(10);  
   
-  if (debugSerial) {
+#ifdef ADXL362_DEBUG
   temp = SPIreadOneRegister(0x2D);
-  Serial.print(  ", Reg 2D after = "); Serial.println(temp); Serial.println();}
+  Serial.print(  ", Reg 2D after = "); 
+  Serial.println(temp); 
+  Serial.println();
+#endif
 }
-
 
 //
 //  readXData(), readYData(), readZData(), readTemp()
 //  Read X, Y, Z, and Temp registers
 //
-int ADXL362::readXData(){
-  int XDATA = SPIreadTwoRegisters(0x0E);
-  if (debugSerial) {Serial.print(  "XDATA = "); Serial.print(XDATA); }
+int16_t ADXL362::readXData(){
+  int16_t XDATA = SPIreadTwoRegisters(0x0E);
+  
+#ifdef ADXL362_DEBUG
+  Serial.print("XDATA = ");
+  Serial.println(XDATA);
+#endif
+  
   return XDATA;
 }
 
-int ADXL362::readYData(){
-  int YDATA = SPIreadTwoRegisters(0x10);
-  if (debugSerial) {Serial.print(  "\tYDATA = "); Serial.print(YDATA); }
+int16_t ADXL362::readYData(){
+  int16_t YDATA = SPIreadTwoRegisters(0x10);
+
+#ifdef ADXL362_DEBUG
+  Serial.print("\tYDATA = "); 
+  Serial.println(YDATA);
+#endif
+  
   return YDATA;
 }
 
-int ADXL362::readZData(){
-  int ZDATA = SPIreadTwoRegisters(0x12);
-  if (debugSerial) {Serial.print(  "\tZDATA = "); Serial.print(ZDATA); }
+int16_t ADXL362::readZData(){
+  int16_t ZDATA = SPIreadTwoRegisters(0x12);
+
+#ifdef ADXL362_DEBUG
+  Serial.print("\tZDATA = "); 
+  Serial.println(ZDATA);
+#endif
+
   return ZDATA;
 }
 
-int ADXL362::readTemp(){
-  int TEMP = SPIreadTwoRegisters(0x14);
-  if (debugSerial) {Serial.print("\tTEMP = "); Serial.print(TEMP); }
+int16_t ADXL362::readTemp(){
+  int16_t TEMP = SPIreadTwoRegisters(0x14);
+
+#ifdef ADXL362_DEBUG
+  Serial.print("\tTEMP = "); 
+  Serial.println(TEMP);
+#endif
+
+  return TEMP;
 }
 
-void ADXL362::readXYZTData(int XData, int YData, int ZData, int Temperature){
+void ADXL362::readXYZTData(int16_t &XData, int16_t &YData, int16_t &ZData, int16_t &Temperature){
+    // burst SPI read
+    // A burst read of all three axis is required to guarantee all measurements correspond to same sample time
+    digitalWrite(slaveSelectPin, LOW);
+    SPI.transfer(0x0B);  // read instruction
+    SPI.transfer(0x0E);  // Start at XData Reg
+    XData = SPI.transfer(0x00);
+    XData = XData + (SPI.transfer(0x00) << 8);
+    YData = SPI.transfer(0x00);
+    YData = YData + (SPI.transfer(0x00) << 8);
+    ZData = SPI.transfer(0x00);
+    ZData = ZData + (SPI.transfer(0x00) << 8);
+    Temperature = SPI.transfer(0x00);
+    Temperature = Temperature + (SPI.transfer(0x00) << 8);
+    digitalWrite(slaveSelectPin, HIGH);
   
-  // burst SPI read
-  // A burst read of all three axis is required to guarantee all measurements correspond to same sample time
-  digitalWrite(slaveSelectPin, LOW);
-  SPI.transfer(0x0B);  // read instruction
-  SPI.transfer(0x0E);  // Start at XData Reg
-  XData = SPI.transfer(0x00);
-  XData = XData + (SPI.transfer(0x00) << 8);
-  YData = SPI.transfer(0x00);
-  YData = YData + (SPI.transfer(0x00) << 8);
-  ZData = SPI.transfer(0x00);
-  ZData = ZData + (SPI.transfer(0x00) << 8);
-  Temperature = SPI.transfer(0x00);
-  Temperature = Temperature + (SPI.transfer(0x00) << 8);
-  digitalWrite(slaveSelectPin, HIGH);
-  
-  if (debugSerial) {
-	Serial.print(  "XDATA = "); Serial.print(XData); 
-	Serial.print(  "\tYDATA = "); Serial.print(YData); 
-	Serial.print(  "\tZDATA = "); Serial.print(ZData); 
-	Serial.print(  "\tTemperature = "); Serial.println(Temperature);
-	}
-
+#ifdef ADXL362_DEBUG
+  Serial.print("XDATA = "); Serial.print(XData); 
+  Serial.print("\tYDATA = "); Serial.print(YData); 
+  Serial.print("\tZDATA = "); Serial.print(ZData); 
+  Serial.print("\tTemperature = "); Serial.println(Temperature);
+#endif
 }
 
-
-
-void ADXL362::setupDCActivityInterrupt(int threshold, byte time){
+void ADXL362::setupDCActivityInterrupt(int16_t threshold, byte time){
   //  Setup motion and time thresholds
   SPIwriteTwoRegisters(0x20, threshold);
   SPIwriteOneRegister(0x22, time);
@@ -126,14 +154,14 @@ void ADXL362::setupDCActivityInterrupt(int threshold, byte time){
   SPIwriteOneRegister(0x27, ACT_INACT_CTL_Reg);       // Write new reg value 
   ACT_INACT_CTL_Reg = SPIreadOneRegister(0x27);       // Verify properly written
 
-  if (debugSerial) {
-  Serial.print("DC Activity Threshold set to ");  	Serial.print(SPIreadTwoRegisters(0x20));
-  Serial.print(", Time threshold set to ");  		Serial.print(SPIreadOneRegister(0x22)); 
-  Serial.print(", ACT_INACT_CTL Register is ");  	Serial.println(ACT_INACT_CTL_Reg, HEX);
-  }
+#ifdef ADXL362_DEBUG
+  Serial.print("DC Activity Threshold set to ");    Serial.print(SPIreadTwoRegisters(0x20));
+  Serial.print(", Time threshold set to ");     Serial.print(SPIreadOneRegister(0x22)); 
+  Serial.print(", ACT_INACT_CTL Register is ");   Serial.println(ACT_INACT_CTL_Reg, HEX);
+#endif
 }
 
-void ADXL362::setupACActivityInterrupt(int threshold, byte time){
+void ADXL362::setupACActivityInterrupt(int16_t threshold, byte time){
   //  Setup motion and time thresholds
   SPIwriteTwoRegisters(0x20, threshold);
   SPIwriteOneRegister(0x22, time);
@@ -144,14 +172,14 @@ void ADXL362::setupACActivityInterrupt(int threshold, byte time){
   SPIwriteOneRegister(0x27, ACT_INACT_CTL_Reg);       // Write new reg value 
   ACT_INACT_CTL_Reg = SPIreadOneRegister(0x27);       // Verify properly written
 
- if (debugSerial) {  
-  Serial.print("AC Activity Threshold set to ");  	Serial.print(SPIreadTwoRegisters(0x20));
-  Serial.print(", Time Activity set to ");  		Serial.print(SPIreadOneRegister(0x22));  
+#ifdef ADXL362_DEBUG
+  Serial.print("AC Activity Threshold set to ");    Serial.print(SPIreadTwoRegisters(0x20));
+  Serial.print(", Time Activity set to ");      Serial.print(SPIreadOneRegister(0x22));  
   Serial.print(", ACT_INACT_CTL Register is ");  Serial.println(ACT_INACT_CTL_Reg, HEX);
-  }
+#endif
 }
 
-void ADXL362::setupDCInactivityInterrupt(int threshold, int time){
+void ADXL362::setupDCInactivityInterrupt(int16_t threshold, int16_t time){
   // Setup motion and time thresholds
   SPIwriteTwoRegisters(0x23, threshold);
   SPIwriteTwoRegisters(0x25, time);
@@ -162,15 +190,14 @@ void ADXL362::setupDCInactivityInterrupt(int threshold, int time){
   SPIwriteOneRegister(0x27, ACT_INACT_CTL_Reg);        // Write new reg value 
   ACT_INACT_CTL_Reg = SPIreadOneRegister(0x27);        // Verify properly written
 
- if (debugSerial) {
+#ifdef ADXL362_DEBUG
   Serial.print("DC Inactivity Threshold set to ");  Serial.print(SPIreadTwoRegisters(0x23));
   Serial.print(", Time Inactivity set to ");  Serial.print(SPIreadTwoRegisters(0x25));
   Serial.print(", ACT_INACT_CTL Register is ");  Serial.println(ACT_INACT_CTL_Reg, HEX);
-  }
+#endif
 }
 
-
-void ADXL362::setupACInactivityInterrupt(int threshold, int time){
+void ADXL362::setupACInactivityInterrupt(int16_t threshold, int16_t time){
   //  Setup motion and time thresholds
   SPIwriteTwoRegisters(0x23, threshold);
   SPIwriteTwoRegisters(0x25, time);
@@ -181,46 +208,43 @@ void ADXL362::setupACInactivityInterrupt(int threshold, int time){
   SPIwriteOneRegister(0x27, ACT_INACT_CTL_Reg);        // Write new reg value 
   ACT_INACT_CTL_Reg = SPIreadOneRegister(0x27);        // Verify properly written
 
- if (debugSerial) {
+#ifdef ADXL362_DEBUG
   Serial.print("AC Inactivity Threshold set to ");  Serial.print(SPIreadTwoRegisters(0x23));
   Serial.print(", Time Inactivity set to ");  Serial.print(SPIreadTwoRegisters(0x25)); 
   Serial.print(", ACT_INACT_CTL Register is ");  Serial.println(ACT_INACT_CTL_Reg, HEX);
-  }
+#endif
 }
 
-
 void ADXL362::checkAllControlRegs(){
-	//byte filterCntlReg = SPIreadOneRegister(0x2C);
-	//byte ODR = filterCntlReg & 0x07;  Serial.print("ODR = ");  Serial.println(ODR, HEX);
-	//byte ACT_INACT_CTL_Reg = SPIreadOneRegister(0x27);      Serial.print("ACT_INACT_CTL_Reg = "); Serial.println(ACT_INACT_CTL_Reg, HEX);
+  //byte filterCntlReg = SPIreadOneRegister(0x2C);
+  //byte ODR = filterCntlReg & 0x07;  Serial.print("ODR = ");  Serial.println(ODR, HEX);
+  //byte ACT_INACT_CTL_Reg = SPIreadOneRegister(0x27);      Serial.print("ACT_INACT_CTL_Reg = "); Serial.println(ACT_INACT_CTL_Reg, HEX);
   digitalWrite(slaveSelectPin, LOW);
   SPI.transfer(0x0B);  // read instruction
   SPI.transfer(0x20);  // Start burst read at Reg 20
-  Serial.println("Start Burst Read of all Control Regs - Library version 6-24-2012:");
-  Serial.print("Reg 20 = "); 	Serial.println(SPI.transfer(0x00), HEX);
-  Serial.print("Reg 21 = "); 	Serial.println(SPI.transfer(0x00), HEX);
-  Serial.print("Reg 22 = "); 	Serial.println(SPI.transfer(0x00), HEX);
-  Serial.print("Reg 23 = "); 	Serial.println(SPI.transfer(0x00), HEX);
-  Serial.print("Reg 24 = "); 	Serial.println(SPI.transfer(0x00), HEX);
-  Serial.print("Reg 25 = "); 	Serial.println(SPI.transfer(0x00), HEX);
-  Serial.print("Reg 26 = "); 	Serial.println(SPI.transfer(0x00), HEX);
-  Serial.print("Reg 27 = "); 	Serial.println(SPI.transfer(0x00), HEX);
-  Serial.print("Reg 28 = "); 	Serial.println(SPI.transfer(0x00), HEX);
-  Serial.print("Reg 29 = "); 	Serial.println(SPI.transfer(0x00), HEX);
-  Serial.print("Reg 2A = "); 	Serial.println(SPI.transfer(0x00), HEX);
-  Serial.print("Reg 2B = "); 	Serial.println(SPI.transfer(0x00), HEX);
-  Serial.print("Reg 2C = "); 	Serial.println(SPI.transfer(0x00), HEX);
-  Serial.print("Reg 2D = "); 	Serial.println(SPI.transfer(0x00), HEX);
-  Serial.print("Reg 2E = "); 	Serial.println(SPI.transfer(0x00), HEX);
-  
+#ifdef ADXL362_DEBUG
+  Serial.println("Start Burst Read of all Control Regs - Library version 6-5-2014:");
+  Serial.print("Reg 20 = ");  Serial.println(SPI.transfer(0x00), HEX);
+  Serial.print("Reg 21 = ");  Serial.println(SPI.transfer(0x00), HEX);
+  Serial.print("Reg 22 = ");  Serial.println(SPI.transfer(0x00), HEX);
+  Serial.print("Reg 23 = ");  Serial.println(SPI.transfer(0x00), HEX);
+  Serial.print("Reg 24 = ");  Serial.println(SPI.transfer(0x00), HEX);
+  Serial.print("Reg 25 = ");  Serial.println(SPI.transfer(0x00), HEX);
+  Serial.print("Reg 26 = ");  Serial.println(SPI.transfer(0x00), HEX);
+  Serial.print("Reg 27 = ");  Serial.println(SPI.transfer(0x00), HEX);
+  Serial.print("Reg 28 = ");  Serial.println(SPI.transfer(0x00), HEX);
+  Serial.print("Reg 29 = ");  Serial.println(SPI.transfer(0x00), HEX);
+  Serial.print("Reg 2A = ");  Serial.println(SPI.transfer(0x00), HEX);
+  Serial.print("Reg 2B = ");  Serial.println(SPI.transfer(0x00), HEX);
+  Serial.print("Reg 2C = ");  Serial.println(SPI.transfer(0x00), HEX);
+  Serial.print("Reg 2D = ");  Serial.println(SPI.transfer(0x00), HEX);
+  Serial.print("Reg 2E = ");  Serial.println(SPI.transfer(0x00), HEX);
+#endif
   digitalWrite(slaveSelectPin, HIGH);
 }
 
-
-
 // Basic SPI routines to simplify code
 // read and write one register
-
 byte ADXL362::SPIreadOneRegister(byte regAddress){
   byte regValue = 0;
   
@@ -242,8 +266,8 @@ void ADXL362::SPIwriteOneRegister(byte regAddress, byte regValue){
   digitalWrite(slaveSelectPin, HIGH);
 }
 
-int ADXL362::SPIreadTwoRegisters(byte regAddress){
-  int twoRegValue = 0;
+int16_t ADXL362::SPIreadTwoRegisters(byte regAddress){
+  int16_t twoRegValue = 0;
   
   digitalWrite(slaveSelectPin, LOW);
   SPI.transfer(0x0B);  // read instruction
@@ -254,8 +278,8 @@ int ADXL362::SPIreadTwoRegisters(byte regAddress){
 
   return twoRegValue;
 }  
-void ADXL362::SPIwriteTwoRegisters(byte regAddress, int twoRegValue){
-  
+
+void ADXL362::SPIwriteTwoRegisters(byte regAddress, int16_t twoRegValue){
   byte twoRegValueH = twoRegValue >> 8;
   byte twoRegValueL = twoRegValue;
   
--- arduino/Accelerometer1/Accelerometer1.ino
+++ arduino/Accelerometer1/Accelerometer1.ino
@@ -2,68 +2,73 @@
  ADXL362_SimpleRead.ino -  Simple XYZ axis reading example
  for Analog Devices ADXL362 - Micropower 3-axis accelerometer
  go to http://www.analog.com/ADXL362 for datasheet
- 
- 
- License: CC BY-SA 3.0: Creative Commons Share-alike 3.0. Feel free 
+
+
+ License: CC BY-SA 3.0: Creative Commons Share-alike 3.0. Feel free
  to use and abuse this code however you'd like. If you find it useful
  please attribute, and SHARE-ALIKE!
- 
+
  Created June 2012
  by Anne Mahaffey - hosted on http://annem.github.com/ADXL362
 
 Connect SCLK, MISO, MOSI, and CSB of ADXL362 to
-SCLK, MISO, MOSI, and DP 10 of Arduino 
+SCLK, MISO, MOSI, and DP 10 of Arduino
 (check http://arduino.cc/en/Reference/SPI for details)
- 
-*/ 
+*/
 
 #include <SPI.h>
 #include "ADXL362.h"
 
-#define NUM_CAL_READINGS 15
+#define DEBUG 0
+#define NUM_AVG_READINGS 3
 
 ADXL362 xl;
 
-int temp;
-int xValue, yValue, zValue, Temperature;
-int xOffset, yOffset, zOffset;
+int lastXval;
+int lastYval;
+int lastZval;
 
-
-unsigned long time = 0;
-
-int lastXval = 0;
-int lastYval = 0;
-int lastZval = 0;
+int xOffset = 0;
+int yOffset = 0;
+int zOffset = 0;
 
 void setup(){
     Serial.begin(9600);
     xl.begin();                   // Setup SPI protocol, issue device soft reset
-    xl.beginMeasure();            // Switch ADXL362 to measure mode  
-    xl.checkAllControlRegs();     // Burst Read all Control Registers, to check for proper setup
+    xl.beginMeasure();            // Switch ADXL362 to measure mode
+    // xl.checkAllControlRegs();     // Burst Read all Control Registers, to check for proper setup
     delay(100);
-    
+
     // set initial readings
     lastXval = xl.readXData();
     lastYval = xl.readYData();
     lastZval = xl.readZData();
 }
 
-void loop(){
-    time++;
-
+void loop() {
     int xSums = 0;
     int ySums = 0;
     int zSums = 0;
 
+    int xReadings[NUM_AVG_READINGS];
+    int yReadings[NUM_AVG_READINGS];
+    int zReadings[NUM_AVG_READINGS];
+
     // Take 3 samples to average out jitter
-    for (int counter = 0; counter < 3; counter++)
+    for (int counter = 0; counter < NUM_AVG_READINGS; counter++)
     {
-      xSums += xl.readXData();
-      ySums += xl.readYData();
-      zSums += xl.readZData();
+        xReadings[counter] = xl.readXData();
+        yReadings[counter] = xl.readYData();
+        zReadings[counter] = xl.readZData();
     }
 
     // Calculate average (which is the current values) of those 3 readings
+    for (int counter = 0; counter < NUM_AVG_READINGS; counter++)
+    {
+        xSums += xReadings[counter];
+        ySums += yReadings[counter];
+        zSums += zReadings[counter];
+    }
     int currentX = xSums / 3;
     int currentY = ySums / 3;
     int currentZ = zSums / 3;
@@ -73,16 +78,29 @@ void loop(){
     int yDiff = currentY - lastYval;
     int zDiff = currentZ - lastZval;
 
-    unsigned int actualMovement = abs(xDiff)+abs(yDiff)+abs(zDiff);
+    unsigned int actualMovement = abs(xDiff) + abs(yDiff) + abs(zDiff);
+
+    if (DEBUG) {
+        for (int counter = 0; counter < NUM_AVG_READINGS; counter++)
+        {
+            Serial.print(xReadings[counter]);
+            Serial.print(",");
+            Serial.print(yReadings[counter]);
+            Serial.print(",");
+            Serial.println(zReadings[counter]);
+        }
+        // Serial.print("RawX:");
+        // Serial.print(xReadings);
+        // Serial.print("\tRawY:");
+        // Serial.print(yReadings);
+        // Serial.print("\tRawZ:");
+        // Serial.print(zReadings);
+        // Serial.print("\tMovement:");
+    }
 
     // print results
-    Serial.print (time);
-    Serial.print (",");
-    Serial.print(actualMovement);
-    Serial.print ("\n");
-    // Serial.println();
-    
-    
+    Serial.println(actualMovement);
+
     // Now that we've printed the differentials,
     // move our current Values into lastVals
     lastXval = currentX;
